24b1ff30829ba3f74eed7d25affb68de5baa794a
springkill <zzz.love.study@gmail.com>
2025-03-11 23:30:36 +0800

    fix: Fix sink finder sorting bug #5 #12

--- src/main/kotlin/org/skgroup/securityinspector/ui/IssueViewWindow.kt
+++ src/main/kotlin/org/skgroup/securityinspector/ui/IssueViewWindow.kt
@@ -9,6 +9,7 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
+import com.intellij.ui.components.JBPanel
 import com.intellij.ui.components.JBScrollPane
 import com.intellij.ui.content.ContentFactory
 import com.intellij.ui.table.JBTable
@@ -19,8 +20,8 @@ import java.awt.BorderLayout
 import java.awt.event.MouseAdapter
 import java.awt.event.MouseEvent
 import javax.swing.JButton
-import javax.swing.JPanel
 import javax.swing.table.DefaultTableModel
+import javax.swing.table.TableRowSorter
 
 /**
  * 类描述：IssueViewWindow 类用于创建SinkFinder。
@@ -31,14 +32,18 @@ import javax.swing.table.DefaultTableModel
 class IssueViewWindow : ToolWindowFactory {
 
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
-        val panel = JPanel(BorderLayout())
-        val tableModel = DefaultTableModel(arrayOf("File", "Line", "Type", "SubType", "CallMode"), 0)
+        val panel = JBPanel<JBPanel<*>>(BorderLayout())
+        val tableModel =
+            DefaultTableModel(arrayOf("File", "Line", "SinkClass", "SinkMethod", "Type", "SubType", "CallMode"), 0)
         val refreshButton = JButton("Init Sink")
         val table = object : JBTable(tableModel) {
             override fun isCellEditable(row: Int, column: Int): Boolean {
                 return false
             }
         }
+
+        val sorter = TableRowSorter(tableModel)
+        table.rowSorter = sorter
 
         table.apply {
             columnModel.getColumn(0).cellRenderer = FirstColumnRenderer()
@@ -49,9 +54,9 @@ class IssueViewWindow : ToolWindowFactory {
                     if (e.clickCount == 2) {
                         val selectedRow = table.selectedRow
                         if (selectedRow != -1) {
-
-                            val file = tableModel.getValueAt(selectedRow, 0) as VirtualFile
-                            val line = tableModel.getValueAt(selectedRow, 1).toString().toInt()
+                            val modelRow = table.rowSorter.convertRowIndexToModel(selectedRow)
+                            val file = tableModel.getValueAt(modelRow, 0) as VirtualFile
+                            val line = tableModel.getValueAt(modelRow, 1).toString().toInt()
 
                             OpenFileDescriptor(project, file, line, 0).navigate(true)
                         }
@@ -69,8 +74,19 @@ class IssueViewWindow : ToolWindowFactory {
                             ApplicationManager.getApplication().invokeLater {
                                 tableModel.rowCount = 0
                                 issues.forEach { issue ->
-                                    tableModel.addRow(arrayOf(issue.file, issue.line.toString(), issue.type, issue.subType, issue.callMode))
+                                    tableModel.addRow(
+                                        arrayOf(
+                                            issue.file,
+                                            issue.line.toString(),
+                                            issue.sinkClass,
+                                            issue.sinkMethod,
+                                            issue.type,
+                                            issue.subType,
+                                            issue.callMode
+                                        )
+                                    )
                                 }
+                                sorter.sort()
                             }
                         })
                     }
